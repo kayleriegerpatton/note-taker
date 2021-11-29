30d69bbd650c7f3be5e88aa03ed1243d29c4cade
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-11-29 14:14:13 -0600

    getAllNotes fn

--- src/controllers/api/notes.js
+++ src/controllers/api/notes.js
@@ -1,13 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+
+// api/notes
 const getAllNotes = (req, res) => {
-  // res.send("getAllNotes function");
   // read from json file
+  const notesJSON = fs.readFileSync(
+    path.resolve(__dirname, "../../../db/db.json"),
+    "utf-8"
+  );
+
   // parse
+  const notes = JSON.parse(notesJSON);
+  console.log(notes);
+
   // return [notes]
-  res.json();
-  //
+  res.json(notes);
 };
 
 const createNote = (req, res) => {
+  res.send("createNote function");
   // validate post body
   // create new note object
   // use uuid library for id#
@@ -18,10 +29,11 @@ const createNote = (req, res) => {
 };
 
 const deleteANote = (req, res) => {
+  res.send("deleteANote function");
   // read from json
   // check if exists
   // extract id from req
-  const { id } = req.params;
+  // const { id } = req.params;
   //   return 404 error if doesn't exist
   // remove note w/ filter
   // write to json file
--- src/controllers/view/notes.js
+++ src/controllers/view/notes.js
@@ -1,9 +1,11 @@
 const path = require("path");
 
 const renderNotes = (fileName) => (req, res) => {
+  res.send("renderNotes function");
+
   // send file to page
-  const filePath = path.join(__dirname, `../../../public/${fileName}.html`);
-  res.sendFile(filePath);
+  //   const filePath = path.join(__dirname, `../../../public/${fileName}.html`);
+  //   res.sendFile(filePath);
 };
 
 module.exports = { renderNotes };
