c55a2ac5a7f8acdd0105b67e62b349e35a141613
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-12-01 20:34:35 -0600

    update port for heroku

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const routes = require("./routes");
 
 const app = express();
-const PORT = 3010;
+const PORT = process.env.PORT || 3010;
 
 app.use(express.static("public"));
 
